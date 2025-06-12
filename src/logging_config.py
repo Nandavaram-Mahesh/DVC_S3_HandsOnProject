9ced7ffb235c161748219086f04b80991632fb62
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2025-06-12 12:01:06 +0530

    Added logging_config and Code for Data Ingestion.

---
+++ src/logging_config.py
@@ -0,0 +1,30 @@
+import os
+import logging
+
+
+def setup_logger(name):
+        
+    # Ensure the "logs" directory exists
+    log_dir = 'logs'
+    os.makedirs(log_dir, exist_ok=True)
+
+    # logging configuration
+    logger = logging.getLogger('data_ingestion')
+    logger.setLevel('DEBUG')
+    if not logger.handlers:
+        # Console Handler
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel('DEBUG')
+        # File Handler
+        log_file_path = os.path.join(log_dir, 'data_ingestion.log')
+        file_handler = logging.FileHandler(log_file_path)
+        file_handler.setLevel('DEBUG')
+        # Formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s- %(filename)s:%(lineno)d - %(message)s')
+        console_handler.setFormatter(formatter)
+        file_handler.setFormatter(formatter)
+        # Add Handlers
+        logger.addHandler(console_handler)
+        logger.addHandler(file_handler)
+
+    return logger
